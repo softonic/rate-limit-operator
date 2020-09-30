1485fb9436eae5252d6941901521ef18e70bb034
Santiago Núñez-Cacho Solans <snunezcacho@gmail.com>
2020-09-30 19:38:19 +0200

    add loglevel

---
+++ pkg/log/level.go
@@ -0,0 +1,10 @@
+package log
+
+// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-instrumentation/logging.md
+
+const WARNING = 0
+const NOTICE = 1
+const INFO = 2
+const EXTENDED = 3
+const DEBUG = 4
+const TRACE = 5

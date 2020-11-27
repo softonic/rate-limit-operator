cd0a324293a42a94fe2aa4b7328df57e3eb56346
Santiago Núñez-Cacho Solans <snunezcacho@gmail.com>
2020-11-27 09:40:51 +0100

    service and address needs to have the same name

--- controllers/envoyfilterobject.go
+++ controllers/envoyfilterobject.go
@@ -70,7 +70,7 @@ func (r *RateLimitReconciler) prepareUpdateEnvoyFilterObjects(rateLimitInstance 
 		ApplyTo:               "CLUSTER",
 		RawConfig:             rawConfigCluster,
 		TypeConfigObjectMatch: "Cluster",
-		ClusterEndpoint:       address,
+		ClusterEndpoint:       fqdn,
 		Labels:                labels,
 	}
 
@@ -86,7 +86,7 @@ func (r *RateLimitReconciler) prepareUpdateEnvoyFilterObjects(rateLimitInstance 
 
 	domain := baseName
 
-	payload = []byte(fmt.Sprintf(`{"config":{"domain":"%s","rate_limit_service":{"grpc_service":{"envoy_grpc":{"cluster_name":"%s"},"timeout":"1.25s"}}},"name":"envoy.rate_limit"}`, domain, address))
+	payload = []byte(fmt.Sprintf(`{"config":{"domain":"%s","rate_limit_service":{"grpc_service":{"envoy_grpc":{"cluster_name":"rate_limit_service"},"timeout":"1.25s"}}},"name":"envoy.rate_limit"}`, domain))
 
 	rawConfigHTTPFilter := json.RawMessage(payload)
 
